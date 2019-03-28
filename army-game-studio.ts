fec982629ad4447bca7ceba5404a989388894734
BlakeleyKilgore <bkilgore@digium.com>
2019-03-27 19:28:23 -0500

    Compare the possible palindrome with its reverse
    This will return true or false.

--- army-game-studio.ts
+++ army-game-studio.ts
@@ -1,6 +1,11 @@
 function checkPalindrome(textInput: string) {
   const possiblePalindrome = textInput.toLowerCase();
   const possiblePalindromeReversed = possiblePalindrome.split('').reverse().join('');
+  if (possiblePalindrome === possiblePalindromeReversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const inputString = 'Hannah';
